2168d845b86f341db1485c07b87f730d5fb4003f
Daniel Tull <dt@danieltull.co.uk>
2014-06-13 16:09:59 +0100

    Initial setup

--- Multidimensional Index Path Test/ViewController.h
+++ Multidimensional Index Path Test/ViewController.h
@@ -1,13 +1,12 @@
 //
-//  AppDelegate.m
+//  ViewController.h
 //  Multidimensional Index Path Test
 //
 //  Created by Daniel Tull on 13/06/2014.
 //  Copyright (c) 2014 Daniel Tull. All rights reserved.
 //
 
-#import "AppDelegate.h"
+@import UIKit;
 
-@implementation AppDelegate
-
+@interface ViewController : UICollectionViewController
 @end
--- Multidimensional Index Path Test/ViewController.m
+++ Multidimensional Index Path Test/ViewController.m
@@ -1,13 +1,17 @@
 //
-//  AppDelegate.m
+//  ViewController.m
 //  Multidimensional Index Path Test
 //
 //  Created by Daniel Tull on 13/06/2014.
 //  Copyright (c) 2014 Daniel Tull. All rights reserved.
 //
 
-#import "AppDelegate.h"
+#import "ViewController.h"
 
-@implementation AppDelegate
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
 
 @end
